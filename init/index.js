f03965ada01fc9944f2c6acce0ab118cd3addc85
Nilam Sutar <nilam.vsp@gmail.com>
2026-05-09 10:28:53 +0530

    Database initialized

--- init/index.js
+++ init/index.js
@@ -1,26 +1,25 @@
+if (process.env.NODE_ENV != "production") {
+    require("dotenv").config({ path: "../.env" });
+}
+
 const mongoose = require("mongoose");
-const initData = require("./data");
-const Listing = require("../models/listing");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
 
 main()
-    .then(()=>{
-        console.log("connected to mongoose succesfully");        
-    })
-    .catch((err)=>{
-        console.log(err);
-        
-    })
+.then(() => {
+    console.log("connected");
+})
+.catch(err => console.log(err));
 
-async function main(){
-   await mongoose.connect('mongodb://127.0.0.1:27017/Wanderlust');
+async function main() {
+    await mongoose.connect(process.env.ATLASDB_URL);
 }
 
-const initDB = async ()=>{
+const initDB = async () => {
     await Listing.deleteMany({});
-    initData.data = initData.data.map((obj)=>({...obj, owner:"69fa04a5e236c61603124a53"}));
     await Listing.insertMany(initData.data);
-    console.log("Data was initialized Successfully!!");
-    
-}
+    console.log("data initialized");
+};
 
 initDB();
