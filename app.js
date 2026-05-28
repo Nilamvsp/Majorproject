82955b54fafb3cb3a13277be83c40a6fbaa50136
Nilamvsp <nilam.vsp@gmail.com>
2026-05-28 10:08:04 +0530

    Add redirect from root to /listings

--- app.js
+++ app.js
@@ -111,6 +111,10 @@ app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
 
+app.get("/", (req, res) => {
+    res.redirect("/listings");
+});
+
 app.use((req, res, next) => {
     next(new ExpressErr(404, "Page Not Found"));
 });
