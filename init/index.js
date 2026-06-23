5654839d64a5948293a0008bba6a10743e0582b8
Nilamvsp <nilam.vsp@gmail.com>
2026-06-23 11:12:49 +0530

    Set owner ID for initialized data
    Modified data initialization to set a specific owner ID.

--- init/index.js
+++ init/index.js
@@ -16,7 +16,7 @@ async function main(){
 }
 
 const initDB = async ()=>{
-    await Listing.deleteMany({});
+   
     initData.data = initData.data.map((obj)=>({...obj, owner:"69fa04a5e236c61603124a53"}));
     await Listing.insertMany(initData.data);
     console.log("Data was initialized Successfully!!");
